4d6e740aebb9942cd57d8342dbd30b2c370c7723
WhatProblem <18355350915@163.com>
2019-04-11 22:07:52 +0800

    表格组件封装

---
+++ src/hotwind/components/table/Table.jsx
@@ -0,0 +1,94 @@
+import React from 'react'
+import './table.scss'
+
+export default class Table extends React.Component {
+    dealData = (columns, data) => {
+        let propArr = []
+        let resData = []
+        columns.forEach((item, i) => {
+            let { prop } = item
+            propArr.push(prop)
+        })
+        data.forEach((item, i) => {
+            let newObj = {}
+            propArr.forEach((items, index) => {
+                newObj[items] = item[items]
+            })
+            resData.push(newObj)
+        })
+        return resData
+    }
+    del = () => {
+        if (this.props.onDel) {
+            this.props.onDel()
+        }
+    }
+    edit = () => {
+        if (this.props.onEdit) {
+            this.props.onEdit()
+        }
+    }
+    render() {
+        const { columns, data, width = '720', align = 'center', defaultWidth = '120', dosign = true } = this.props
+        const datas = this.dealData(columns, data)
+        return (
+            <div className="table" >
+                <div className="tableHeader">
+                    <table cellSpacing="0" cellPadding="0" border="0" className="htable">
+                        <colgroup>
+                            {
+                                columns.map((item, i) =>
+                                    <col width={item.width || defaultWidth} key={i} style={{ width: item.width || defaultWidth }} />
+                                )
+                            }
+                            {dosign && <col style={{ width: '160px' }} />}
+                        </colgroup>
+                        <thead className="thead">
+                            <tr className="rhead">
+                                {
+                                    columns.map((item, i) =>
+                                        <th className="thtitle" colSpan='1' rowSpan='1' key={item.prop}>
+                                            <div className="htitle" style={{ textAlign: align }}>{item.label}</div>
+                                        </th>)
+                                }
+                                {dosign && <th className="thtitle" colSpan='1' rowSpan='1'>
+                                    <div className="htitle" style={{ textAlign: align }}>操作</div>
+                                </th>}
+                            </tr>
+                        </thead>
+                    </table>
+                </div>
+                <div className="tableContent">
+                    <table cellSpacing="0" cellPadding="0" border="0" className="ctable">
+                        <colgroup>
+                            {
+                                columns.map((item, i) =>
+                                    <col width={item.width || defaultWidth} key={i} style={{ width: item.width || defaultWidth }} />
+                                )
+                            }
+                            {dosign && <col style={{ width: '160px' }} />}
+                        </colgroup>
+                        <tbody>
+                            {
+                                datas.map((items, index) => <tr key={index} className="ctr">
+                                    {
+                                        Object.keys(items).map((idx, isd) =>
+                                            <td className="thtitle" key={idx}>
+                                                <div className="htitle" style={{ textAlign: align }}>{items[idx]}</div>
+                                            </td>)
+                                    }
+                                    {dosign && <td className="thtitle">
+                                        <div className="htitle" style={{ textAlign: align }}>
+                                            <button className="edit" onClick={this.edit.bind(this)}>编辑</button>
+                                            <button className="delete" onClick={this.del.bind(this)}>删除</button>
+                                        </div>
+                                    </td>}
+                                </tr>)
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        )
+    }
+}
--- src/hotwind/view/editGoods/EditGoods.jsx
+++ src/hotwind/view/editGoods/EditGoods.jsx
@@ -1,9 +1,87 @@
 import React from 'react'
+import Table from '../../components/table/Table'
+import $http from '../../http'
+import './editGoods.scss'
 
 export default class EditGoods extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            columns: [
+                {
+                    label: "日期",
+                    prop: "date",
+                    width: 180
+                },
+                {
+                    label: "姓名",
+                    prop: "name",
+                    width: 180
+                },
+                {
+                    label: "地址",
+                    prop: "address",
+                    width: 240
+                }
+            ],
+            data: [{
+                date: '2016-05-02',
+                address: '上海市普陀区金沙江路 1518 弄',
+                name: '王小虎',
+            }, {
+                date: '2016-05-04',
+                address: '上海市普陀区金沙江路 1517 弄',
+                name: '王小虎',
+            }, {
+                date: '2016-05-01',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1519 弄'
+            }, {
+                date: '2016-05-03',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1516 弄'
+            }, {
+                date: '2016-05-02',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1518 弄'
+            }, {
+                date: '2016-05-04',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1517 弄'
+            }, {
+                date: '2016-05-01',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1519 弄'
+            }, {
+                date: '2016-05-03',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1516 弄'
+            }]
+        }
+    }
+    getGoods = () => {
+        let param = { pages: 1, offsets: 10 }
+        $http.get('getGoods', param).then(res => {
+            // console.log(res)
+        })
+    }
+    componentWillMount() {
+        this.getGoods();
+    }
+
+    delTable = () => {
+        console.log('删除')
+    }
+    editTable=()=>{
+        console.log('编辑')
+    }
     render() {
         return (
-            <div>EditGoods</div>
+            <div className="editGoods">
+                <div className="editTable">
+                    <Table columns={this.state.columns} onDel={this.delTable.bind(this)} onEdit={this.editTable.bind(this)} align="left" data={this.state.data} />
+                </div>
+            </div>
         )
     }
 }
